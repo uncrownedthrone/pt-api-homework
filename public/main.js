5c2ffb8ad02acac31f519fd9156938e5b151d7b3
Justin Swiger <justin.swiger@me.com>
2019-10-24 18:33:12 -0400

    updated api site

--- public/main.js
+++ public/main.js
@@ -1,7 +1,41 @@
+const punchlineButton = document.querySelector('button#get-joke')
+const newJokeButton = document.querySelector('button#new-joke')
+
+let punchline = ''
+
+const setPunchline = (p) => {
+	console.log(p)
+	punchline = p
+}
+
+const getJoke = () => {
+	fetch('http://numbersapi.com/random/year')
+		.then((response) => {
+			return response.json()
+		})
+		.then((json) => {
+			console.log(json)
+			document.querySelector('h1#setup').textContent = json.setup
+			setPunchline(json.punchline)
+		})
+}
+
+const revealPunchline = () => {
+	document.querySelector('p#punchline').textContent = punchline
+}
+
+const getNewJoke = () => {
+	document.querySelector('p#punchline').textContent = ''
+	getJoke()
+}
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+	getJoke()
+	if (document.querySelector('h1.hello-world')) {
+		document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+	}
+	punchlineButton.addEventListener('click', revealPunchline)
+	newJokeButton.addEventListener('click', getNewJoke)
 }
 
 document.addEventListener('DOMContentLoaded', main)
